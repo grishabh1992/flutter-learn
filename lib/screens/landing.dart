8a044bbce7566ea45df62cc37f63960e81b5fae6
Rishabh Garg <rishabh.garg01@nagarro.com>
2020-03-31 17:53:36 +0530

    Added other Concept  for learning

--- lib/screens/landing.dart
+++ lib/screens/landing.dart
@@ -4,36 +4,34 @@ import 'package:flutter_learn/screens/login.dart';
 
 class LandingPage extends StatelessWidget {
   final List<Item> items = [
-    Item.formObject({ 'name' :'Login Screen', 'screen' : LoginScreen()}), 
-    Item.formObject({ 'name' :'Simple Routing', 'screen' : LoginScreen()}),
-    Item.formObject({ 'name' :'Named Routing', 'screen' : LoginScreen()})
+    Item.formObject({'name': 'Login Screen', 'screen': LoginScreen()}),
+    Item.formObject({'name': 'Simple Routing', 'screen': LoginScreen()}),
+    Item.formObject({'name': 'Named Routing', 'screen': LoginScreen()}),
+    Item.formObject({'name': 'API Calling', 'screen': LoginScreen()}),
+    Item.formObject({'name': 'Authentication', 'screen': LoginScreen()})
   ];
-  
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Center(
-          child: Text('Flutter Demo App')
-        )
-      ),
-      body : ListView.builder(
-        itemBuilder: (BuildContext context, int position) {
-          return Card(
-            color: Colors.white,
-            elevation: 2.0,
-            child: ListTile(
-              title: Text(this.items[position].name.toString()),
-              onTap: () async {
-                debugPrint("Tapped");              
-                  await Navigator.push(context, MaterialPageRoute(builder: (context) => this.items[position].screen));
-              }
-            ),
-          );
-        },
-        itemCount: items.length,
-      )
-    );
+        appBar: AppBar(title: Center(child: Text('Flutter Learning App'))),
+        body: ListView.builder(
+          itemBuilder: (BuildContext context, int position) {
+            return Card(
+              color: Colors.white,
+              elevation: 2.0,
+              child: ListTile(
+                  title: Text(this.items[position].name.toString()),
+                  onTap: () async {
+                    debugPrint("Tapped");
+                    await Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => this.items[position].screen));
+                  }),
+            );
+          },
+          itemCount: items.length,
+        ));
   }
-
 }
